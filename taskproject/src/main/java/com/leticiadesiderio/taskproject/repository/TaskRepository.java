906c018cf8ab5be33ba55f5238990674221cb77a
leticia.desiderio <leticia.desiderio@bnpsolucoes.com.br>
2025-08-12 22:25:26 -0300

    feat(task): add repository

---
+++ taskproject/src/main/java/com/leticiadesiderio/taskproject/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.leticiadesiderio.taskproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leticiadesiderio.taskproject.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{}
